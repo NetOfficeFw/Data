7f002e9c708e1f93dd9c346d3bbf36a65544deb6
Jozef Izso <jozef.izso@gmail.com>
2023-01-22 15:48:46 +0100

    Add script to export info about libraries in Microsoft Word for Mac

---
+++ macos/export.sh
@@ -0,0 +1,29 @@
+##!/usr/bin/env bash
+
+APP='Microsoft PowerPoint'
+
+libs=(
+  OLEAutomation
+  OLE
+  COMBase
+  'Visual Basic for Applications'
+  MsoCore
+  MicrosoftOffice
+  'ADAL-OSX'
+  WLMKernel
+  WLMUser
+)
+
+/usr/libexec/PlistBuddy -c "print CFBundleVersion" "/Applications/$APP.app/Contents/Info.plist" | awk '{print "CFBundleVersion="$1}' > "version.txt"
+/usr/libexec/PlistBuddy -c "print MicrosoftBuildNumber" "/Applications/$APP.app/Contents/Info.plist" | awk '{print "MicrosoftBuildNumber="$1}' >> "version.txt"
+/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" "/Applications/$APP.app/Contents/Info.plist" | awk '{print "CFBundleShortVersionString="$1}' >> "version.txt"
+cp "/Applications/$APP.app/Contents/Info.plist" Info.plist
+plutil -convert xml1 Info.plist
+
+for lib in "${libs[@]}"; do
+  echo $lib
+  mkdir -p "$lib"
+  binpath="/Applications/$APP.app/Contents/Frameworks/$lib.framework/Versions/A/$lib"
+  nm -m "$binpath" | c++filt | cut -c 10- > "$lib/exports.txt"
+  otool -L "$binpath" > "$lib/libraries.txt"
+done
